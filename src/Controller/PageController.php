7267711dc38525146f80b8664892522dfea15175
dlefebvre614 <didier.lefebvre12@gmail.com>
2021-06-16 17:36:49 +0200

    Add work to day in course

---
+++ src/Controller/PageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
+{
+    /**
+     * @Route("/about", name="page_about")
+     */
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig', [
+            'bg_image' => 'about-bg.jpg',
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="page_contact")
+     */
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', [
+            'bg_image' => 'contact-bg.jpg',
+        ]);
+    }
+}
---
+++ src/Controller/PostController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('post/posts.html.twig', [
+            'post' => [
+                'title' => 'Le titre de l\'article',
+                'content' => 'Le contenu de notre article',
+            ],
+            'bg_image' => 'home-bg.jpg',
+        ]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="post_view", methods={"GET"}, requirements={"id"="\d+"})
+     */
+    public function view($id): Response
+    {
+        //dd($id);
+        //var_dump($id);
+        return $this->render('post/post.html.twig', [
+            'post' => [
+                'title' => 'Le titre de l\'article',
+                'content' => 'Le contenu de notre article',
+            ],
+            'bg_image' => 'post-bg.jpg',
+            //'controller_name' => 'Post ID ' . $id . '',
+            //'id' => $id
+        ]);
+    }
+}
